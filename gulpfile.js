7a0b3d5d56153a3886bc85dffaf6d64408ebb66b
sapalenko <sapalenko1996@gmail.com>
2019-03-24 17:17:04 +0300

    updated gulp-file. scss modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,59 @@
+'use strict';
 
-var gulp = require('gulp');
-	sass = require('gulp-sass');
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
 
-// Styles
-/*	gulp.task ('styles', function(){
-		return gulp.src('SASS/*.scss')
-		.pipe (sass({ style: 'expanded',}))
-		.pipe (gulp.dest('CSS'))
-		.pipe (notify({massage: 'styles task comlete'}));
-	});
-	gulp.task('build', ['styles']);
-	gulp.task('watch', ['styles']);
-	gulp.task('default', ['watch']);*/
-	function defaultTask(cb){
-		cb();
-	}
-	exports.default = defaultTask
+var path = {
+    build: {
+        css: 'css/'
+    },
+    src: {
+        style: 'sass/*.scss'
+    },
+    watch: {
+        style: 'sass/*.scss'
+    },
+    clean: './css'
+};
+
+var config = {
+    server: {
+        baseDir: "./css"
+    }
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('css', function () {
+    gulp.src(path.src.style) 
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('watch', function(){
+    gulp.watch([path.watch.style], gulp.series('css'));
+});
+
+
+gulp.task('default', gulp.parallel('css', 'watch'), function(done){done();});
